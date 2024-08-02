9f28d8b8af045154df51e5962cf3096283ee67fd
00gigs <1onlinegigs@gmail.com>
2024-08-02 12:56:10 -0400

    navbar UI cosmetic

--- myqte/src/components/Navbar.js
+++ myqte/src/components/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { navLinks } from '../constants/Navlinks'
 const Navbar = () => {
   return (
-    <div className='mb-7 flex sticky justify-center text-center bg-[#54D449] h-9 items-center border-b-4 border-black sm:h-12'>
-      <ul className='flex gap-2 justify-center mb-1 '>
+    <div className='mb-7 shadow-2xl flex sticky justify-center text-center bg-[#54D449] h-9 items-center border-b-4 border-black sm:h-12'>
+      <ul className='flex  justify-center '>
       {navLinks.map((link)=>(
-        <a href={link.route}>
-          <li className='text-[11.5px]   sm:text-[16px]  '>{link.label}</li>
+        <a className='pt-3 px-0 mb-3 sm:px-[7px]' href={link.route}>
+          <li className='text-[10.5px] shadow-2xl border-l-2  border-r-2  font-serif text-center py-5 bg-blue-200 h-2 rounded-xl px-2  border-black hover:bg-blue-400 hover:scale-y-110 sm:text-[16px]'>{link.label}</li>
         </a>
       ))}
       </ul>
